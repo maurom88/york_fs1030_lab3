ea98fdf9f07c6dc8e16411925aab59eae5243fc7
Mauro Meden <mauro.meden@gmail.com>
2022-08-10 15:02:58 -0600

    Comment out body parser

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import bodyParser from 'body-parser';
+// import bodyParser from 'body-parser';
 import mysql2 from 'mysql2';
 import cors from 'cors';
 import dotenv from 'dotenv';
@@ -16,8 +16,8 @@ dotenv.config();
 const port = process.env.port || 5000;
 
 // configure middleware
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json()); // parse form data client
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.json()); // parse form data client
 app.use(cors())
 app.use('/job_seekers', jobSeekersRoutes);
 app.use('/jobs', jobsRoutes);
